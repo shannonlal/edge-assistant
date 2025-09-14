757f96a6b0064534706de2596fb7b1f6ecdc5b11
Shannon Lal <shannonlal@hotmail.com>
2025-09-14 19:52:07 -0400

    Update Latest Configuration layout for this

---
+++ src/pages/hello.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { HelloWorld } from '@/components/HelloWorld';
+
+const HelloPage: React.FC = () => {
+  return (
+      <div className="container mx-auto px-4 py-8">
+        <HelloWorld />
+      </div>
+  );
+};
+
+export default HelloPage;
